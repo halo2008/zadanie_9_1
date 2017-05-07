05a5b7ddbd8107002b1414d4b1fa41d690f01740
halo2008 <ksedkowski@gmail.com>
2017-05-07 13:04:43 +0200

    porpawki do instrukcji warnukowej if

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function getTriangleArea(a,h) {
     console.log('błedna wartosc');
     return;
   }
-  else (a > 0 && h > 0){
+  else {
     var value = (a * h) / 2;
     console.log('pole trojkata ' + value)
     return;
@@ -13,4 +13,3 @@ function getTriangleArea(a,h) {
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(2, 15);
 var triangle3Area = getTriangleArea(3, 15);
-console.log(getTriangleArea());
